ce4940d85f1f295c2a4b33d2eb418d4331355b1e
omar hosny <omarhosnystar@gmail.com>
2020-07-05 10:16:33 +0200

    Deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const cors = require("cors");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
 const passport = require("passport");
@@ -19,6 +20,8 @@ require("./config/passport")(passport);
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
@@ -85,6 +88,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
 app.use("/stories", require("./routes/stories"));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 
